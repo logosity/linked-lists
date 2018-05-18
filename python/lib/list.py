805202c4e531f6d99a0900f6c4cbf2a12e1f0166
Bill Caputo <github@williamcaputo.com>
2018-05-17 23:57:16 -0500

    rand test written and failing

--- python/lib/list.py
+++ python/lib/list.py
@@ -2,10 +2,15 @@ class Node():
     def __init__(self, value):
         self._value = value
         self._next = None
+        self._rand = None
 
     @property
     def value(self):
         return self._value
+
+    @value.setter
+    def value(self, value):
+        self._value = value
 
     @property
     def next(self):
@@ -15,7 +20,25 @@ class Node():
     def next(self, next_node):
         self._next = next_node
 
+    @property
+    def rand(self):
+        return self._rand
+
+    @rand.setter
+    def rand(self, rand_node):
+        self._rand = rand_node
+
     def clone(self):
         new = Node(self.value)
-        new.next = self.next
+
+        if self.next is self:
+            new.next = new
+            return new
+
+        if self.next is not None:
+            new.next = self.next.clone()
+            return new
+
         return new
+
+
--- python/lib/test/test_list.py
+++ python/lib/test/test_list.py
@@ -5,45 +5,98 @@ from sure import expect, assertion
 
 from lib.list import Node
 
-#@unittest.skip("WIP")
+    #@unittest.skip("WIP")
 
 class CreatingLists(unittest.TestCase):
     def setUp(self):
-        pass
+        self.node = Node("Hello")
 
     def test_single_node_value(self):
-        node = Node("Hello")
-        node.value.should.equal("Hello")
+        self.node.value.should.equal("Hello")
 
     def test_single_node_next(self):
-        node = Node("Hello")
-        node.next.should.equal(None)
+        self.node.next.should.equal(None)
 
-class CloningLists(unittest.TestCase):
+class CloningOneElementLists(unittest.TestCase):
     def setUp(self):
-        pass
+        self.node = Node("Hello")
 
     def test_single_node_value(self):
-        node = Node("Hello")
+        result = self.node.clone()
 
-        result = node.clone()
         result.value.should.equal("Hello")
-        result.should.not_be(node)
+        result.should.not_be(self.node)
+
+    def test_value_after_mutation(self):
+        result = self.node.clone()
+
+        self.node.value = "World"
+
+        result.value.should.equal("Hello")
+        self.node.value.should.equal("World")
 
     def test_single_node_next(self):
-        node = Node("Hello")
+        result = self.node.clone()
 
-        result = node.clone()
         result.next.should.equal(None)
-        result.should.not_be(node)
+        result.should.not_be(self.node)
 
-    @unittest.skip("WIP")
     def test_single_node_self_ref(self):
-        node = Node("Hello")
-        node.next = node
+        self.node.next = self.node
 
-        result = node.clone()
+        result = self.node.clone()
+
         result.value.should.equal("Hello")
-        result.next.should.equal(result)
-        result.should.not_be(node)
+        result.next.should.be(result)
+
+class CloningMultiElements(unittest.TestCase):
+    def setUp(self):
+        self.first = Node("Test1")
+        self.second = Node("Test2")
+        self.third = Node("Test3")
+        self.fourth = Node("Test4")
+
+        self.first.next = self.second
+        self.second.next = self.third
+        self.third.next = self.fourth
+
+    def test_values(self):
+        result = self.first.clone()
+
+        result.value.should.equal("Test1")
+        result.next.value.should.equal("Test2")
+        result.next.next.value.should.equal("Test3")
+        result.next.next.next.value.should.equal("Test4")
+
+    def test_identities(self):
+        result = self.first.clone()
+
+        result.should.not_be(self.first)
+        result.next.should.not_be(self.second)
+        result.next.next.should.not_be(self.third)
+        result.next.next.next.should.not_be(self.fourth)
+
+        result.next.next.next.next.should.equal(None)
+
+class RandomRefs(CloningMultiElements):
+    def setUp(self):
+
+        super().setUp()
+
+        self.first.rand = self.fourth
+        self.fourth.rand = self.first
+        self.second.rand = self.second
+        self.third.rand = self.second
+
+        self.result = self.first.clone()
+        self.new_first = self.result
+        self.new_second = self.result.next
+        self.new_third = self.result.next.next
+        self.new_fourth = self.result.next.next.next
+
+    def test_random_identities(self):
+        self.new_first.rand.should.be(self.new_fourth)
+        self.new_fourth.rand.should.be(self.new_first)
+        self.new_second.rand.should.be(self.new_second)
+        self.new_third.rand.should.be(self.new_second)
 
