948814013d2308134f1de03c157e525791d5fc0d
Bill Caputo <github@williamcaputo.com>
2018-05-19 17:56:36 -0500

    build and test setup for cpp version

---
+++ cpp/spec/list_spec.cpp
@@ -0,0 +1,8 @@
+#include "catch.hpp"
+#include "list.h"
+
+TEST_CASE("Foo", "[foo]") {
+  REQUIRE(foo(0) == 42);
+}
+
+
---
+++ cpp/src/list.cpp
@@ -0,0 +1,6 @@
+#include "list.h"
+using namespace std;
+
+int foo(int a) {
+  return 42;
+}
